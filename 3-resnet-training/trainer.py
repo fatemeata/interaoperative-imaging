e8d265a3198900aaaf2a1ef4890177e347b8f692
fatemeata <fa23.atayi@gmail.com>
2022-12-09 21:36:22 +0100

    modify number of epochs

--- 3-resnet-training/trainer.py
+++ 3-resnet-training/trainer.py
@@ -4,22 +4,21 @@ import torch.cuda
 import wrist_network
 import dataset
 from utils.data_splitting import DataSplitting
-from torch.utils.tensorboard import SummaryWriter
 from pytorch_lightning.loggers import TensorBoardLogger
-
+from utils.data_augmentation import DataAugmentation
 
 if __name__ == "__main__":
 
     # 1. init dataset
     root_dir = "data"
-    wrist_dataset = dataset.WristDataset(root_dir=root_dir)
+    wrist_dataset = dataset.WristDataset(root_dir=root_dir, transform=DataAugmentation())
 
     # 2. split dataset
     train, validation, test = DataSplitting().k_fold_data(data=wrist_dataset)
 
     # 3. instantiate dataloader for train, valid, test dataset
-    train_dataloader = torch.utils.data.DataLoader(train, batch_size=8, shuffle=True, num_workers=0)
-    valid_dataloader = torch.utils.data.DataLoader(validation, batch_size=1, shuffle=False, num_workers=0)
+    train_dataloader = torch.utils.data.DataLoader(train, batch_size=4, shuffle=True, num_workers=0)
+    valid_dataloader = torch.utils.data.DataLoader(validation, batch_size=4, shuffle=True, num_workers=0)
     test_dataloader = torch.utils.data.DataLoader(test, batch_size=1, shuffle=False, num_workers=0)
 
     # 4. create trainer
@@ -30,12 +29,14 @@ if __name__ == "__main__":
     trainer = Trainer(
         accelerator="auto",
         devices=1 if torch.cuda.is_available() else None,
-        max_epochs=50,
+        max_epochs=300,
         log_every_n_steps=30,
         logger=logger
     )
 
     # 5. use trainer to optimize the model, given training and validation dataloader
     trainer.fit(wrist_model, train_dataloader, valid_dataloader)
-    trainer.test(dataloaders=test_dataloader, ckpt_path="best")
+    trainer.test(dataloaders=test_dataloader, ckpt_path="last")
+    predictions = trainer.predict(dataloaders=test_dataloader)
+
 
