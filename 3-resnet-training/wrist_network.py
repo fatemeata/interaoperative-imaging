d0b8dfeb2e2092144cd0fcf81d7f960b75d50ede
fatemeata <fa23.atayi@gmail.com>
2022-12-09 21:37:17 +0100

    modify prediction step

--- 3-resnet-training/wrist_network.py
+++ 3-resnet-training/wrist_network.py
@@ -1,13 +1,8 @@
 import torch
-import torchvision
-import torch.nn as nn
 import torch.nn.functional as F
 import pytorch_lightning
-from typing import Any, Optional
-from utils.resnet_flex import ResNet, myresnet18
-import utils.resnet_flex as resnet
-
-from pytorch_lightning.utilities.types import STEP_OUTPUT
+from typing import Any
+from utils.resnet_flex import myresnet18
 
 
 class WristResNetwork(pytorch_lightning.LightningModule):
@@ -22,42 +17,36 @@ class WristResNetwork(pytorch_lightning.LightningModule):
 
     def training_step(self, batch, *args, **kwargs):
         x = batch["image"]
+        print(x.shape)
         y = (batch["points"].squeeze(axis=0)).squeeze(axis=0)
         train_loss = F.mse_loss(self.forward(x), y)
-        self.log("train_loss", train_loss)
+        self.log("train_loss", train_loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
         return train_loss
 
     def validation_step(self, batch, *args, **kwargs):
         x = batch["image"]
         y = (batch["points"].squeeze(axis=0)).squeeze(axis=0)
         val_loss = F.mse_loss(self.forward(x), y)
-        self.log("val_loss", val_loss)
+        self.log("val_loss", val_loss, on_epoch=True, prog_bar=True, logger=True)
         return val_loss
 
-# TODO: WHAT is the difference between test_step and predict_step?
     def test_step(self, batch, *args, **kwargs):
         x = batch["image"]
         y = (batch["points"].squeeze(axis=0)).squeeze(axis=0)
         y_hat = self.forward(x)
         test_loss = F.mse_loss(y_hat, y)
-        self.log("test_loss", test_loss)
+        self.log("test_loss", test_loss, on_step=True, prog_bar=True, logger=True)
 
         return {'test_loss': test_loss, 'preds': y_hat, 'target': y}
 
-        # trainer = Trainer()
-        # trainer.fit(model)
-
-        # automatically loads the best weights for you
-        # trainer.test(model)
-
     def predict_step(self, batch, batch_idx: int, dataloader_idx: int = 0) -> Any:
-        # enable Monte Carlo Dropout
         if len(batch) == 2:
             x, y = batch
         else:
             x = batch
-        return self(x)
+        pred = self.forward(x)
+        return pred
 
     def configure_optimizers(self):
-        return torch.optim.SGD(self.parameters(), lr=0.0001)
+        return torch.optim.Adam(self.parameters(), lr=0.001)
 
